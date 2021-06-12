a96e01a51aad3580c77bb150dfb414d45b9fdaef
predator-007 <srijanraj2000@gmail.com>
2021-06-12 10:50:59 +0530

    commit

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
-const express=require('express');
-const app=express();
-const http=require('http');
-const server=http.createServer(app);
+const app=require('express')()
+const server=require('http').createServer(app)
 const cors=require('cors');
-const socket=require('socket.io');
-const io =socket(server,{
+const io=require('socket.io')(server,{
     cors:{
-        origin: "*",
-        methods: ["GET","POST"]
+        origin:"*",
+        methods:["GET","POST"]
     }
-}); 
-
-app.use(cors());
-app.get("/",(req,res)=>{
+})
+app.use(cors())
+app.get('/',(req,res)=>{
     res.send("server is running");
-});
+})
+const ns={}
+const sn={}
 
 io.on("connection",(socket)=>{
     
-    socket.emit("me",socket.id);
-
-    socket.on("disconnect",()=>{
-        socket.broadcast.emit("callEnded");
-    });
-
-    
-    socket.on("callUser",(data)=>{
-        io.to(data.userToCall).emit("callUser",{signal:data.signalData,from:data.from,name:data.name});
-    });
-    
-    socket.on("answerCall",(data)=>{
-        io.to(data.to).emit("callAccepted",data.signal);
+    socket.on("connectToSocket",(data)=>{
+        ns[data]=socket.id
+        sn[socket.id]=data
+        console.log(data +" connected")
+        console.log("connected User:")
+        console.log(ns)
+        io.emit("updateUser",ns)
     })
+    
+    socket.on("callUser", (data) => {
+        console.log("calling invoked to",ns[data.to],data.to);
+        io.to(ns[data.to]).emit("callUser", { signal: data.signalData, from: data.from })
+	})
+    
+    socket.on('answerCall',(data)=>{
+        console.log('call answered');
+        io.to(ns[data.to]).emit("callAccepted",data.signal);
+    })
+    socket.on('disconnect',()=>{
+        const name=sn[socket.id];
+        delete sn[socket.id];
+        delete ns[name];
+        console.log(name+" disconnected")
+        console.log(ns)
+        io.emit("updateUser",ns);
 
-});
+    })
+})
 
 
-const PORT=process.env.PORT||5000;
-server.listen(PORT,()=>console.log('server listening on '+PORT));
+const PORT=process.env.PORT||5000
+server.listen(PORT,()=>console.log("server running"));
